106946ae5241d3ec1f5cc6af7fb5d78147f3b216
arissetyawan1 <aris.setyawan354313@gmail.com>
2021-10-12 10:57:38 +0700

    Belajar Dart Programming

---
+++ test.dart
@@ -0,0 +1,16 @@
+main() {
+  print('Hello World!!!');
+  print('My First Dart Language');
+  var a = 1, b = 2;
+  if (a + b == 2) {
+    print("Hasil sama dengan 2");
+  } else {
+    print("Tidak sama dengan 2");
+  }
+  var c = a + b;
+  for (var i = 0; i < c; i++) {
+    if (i.isEven) {
+      print(i);
+    }
+  }
+}
